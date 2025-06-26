fa3c2c57802babaa289105bf4082a55472e5304f
Preet <Preet@gmail.com>
2025-06-26 15:10:56 +0530

    10th

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import *
-from .forms import ProjectForm
+from .forms import ProjectForm, ReviewForm
+from django.contrib import messages
 from django.db.models import Q
 from .utils import searchProjects, paginateProjects
 
@@ -20,7 +21,22 @@ def projects(request):
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
-    return render(request, 'projects/single-project.html', {'project': projectObj})
+    form = ReviewForm()
+    
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = projectObj
+        review.owner = request.user.profile
+        review.save()
+        
+        messages.success(request, 'Submit review')
+        
+        projectObj.voteCount
+        
+        return redirect('project', projectObj.id)
+    
+    return render(request, 'projects/single-project.html', {'project': projectObj, 'form':form})
 
 
 @login_required(login_url='login')
