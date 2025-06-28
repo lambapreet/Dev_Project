141f0884484fc23506f8191ca4fcc01faa548652
Preet <Preet@gmail.com>
2025-06-28 13:25:21 +0530

    12th

--- Dev_Project/settings.py
+++ Dev_Project/settings.py
@@ -121,6 +121,14 @@ USE_I18N = True
 USE_TZ = True
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'pohoyi2615@kimdyn.com'
+EMAIL_HOST_PASSWORD = 'abc1234'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
--- users/signals.py
+++ users/signals.py
@@ -2,6 +2,8 @@ from .models import Profile
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 # @receiver(post_save, sender=Profile)   
@@ -14,6 +16,18 @@ def createProfile(sender, instance, created, **kwargs):
             email = user.email,
             name = user.first_name,
         )
+        
+        subject = 'Welcome to Dev_Project'
+        message = 'We are glad you are here!'
+        
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
+        
         
 def updateUser(sender, instance, created, **kwargs):
     profile = instance
